ee15cc3794dd281cacd7c5b1794358d757536ef4
ГÐUlybin Nikolai <bitmaxml@gmail.com>
2022-11-16 15:17:35 +0300

    add  changes day15

--- src/day15/Task1.java
+++ src/day15/Task1.java
@@ -5,28 +5,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         File file = new File("src/resources/shoes.csv");
         File file1 = new File("missingShoes");
-        try {
-            Scanner scanner = new Scanner(file);
-            try {
-                PrintWriter pw = new PrintWriter(file1);
-                while (scanner.hasNextLine()) {
-                    String[] test = scanner.nextLine().split(";");
-                    if (Integer.parseInt(test[2]) == 0) {
-                        pw.println(Arrays.toString(test));
-                    }
+        try (Scanner scanner = new Scanner(file);
+             PrintWriter pw = new PrintWriter(file1);) {
+
+            while (scanner.hasNextLine()) {
+                String[] test = scanner.nextLine().split(";");
+                if (Integer.parseInt(test[2]) == 0) {
+                    pw.println(test[0] + ", " + test[1] + ", " + test[2]);
                 }
-                pw.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("Ошибка при создании файла");
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println("Файл не найден");
         }
     }
 }
+
 
 
 
